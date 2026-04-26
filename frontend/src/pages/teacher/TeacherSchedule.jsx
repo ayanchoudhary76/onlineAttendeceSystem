9a87e7cceb5a1259827bc411ce5ea8c9cac6535a
ayanchoudhary76 <ayanchoudhary76@gmail.com>
2026-04-27 00:29:44 +0530

    fixed a react app error

--- frontend/src/pages/teacher/TeacherSchedule.jsx
+++ frontend/src/pages/teacher/TeacherSchedule.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useMySchedule } from '../../hooks/useMySchedule';
 import { useAuth } from '../../context/AuthContext';
